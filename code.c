a2450266ca820c52bdfe2f06b4bbc3e46a90a2df
luckysonani <sonani.lucky1234@gmail.com>
2025-06-06 14:07:48 +0530

    code.c is commited and updated

--- code.c
+++ code.c
@@ -1,2 +1,10 @@
 // lucky
-obama=10
+#include <stdlib.h>
+intmain() {
+    int n = 10; // Number of random numbers to generate
+    for (int i = 0; i < n; i++) {
+        int random_number = rand() % 100; // Generate a random number between 0 and 99
+        printf("%d\n", random_number); // Print the random number
+    }
+    return 0;
+}
